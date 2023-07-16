17d7ef805d4bb6065eb6db13e83e9d03f8110791
YousefKullab <the1yousef98@gmail.com>
2023-07-16 22:07:27 +0300

    create get route to return all projectData and create post route ro add incoming data to projectData

--- server.js
+++ server.js
@@ -3,15 +3,14 @@ projectData = {};
 
 /* Dependencies */
 const express = require('express')
-const bodyParser = require('body-parser')
 const cors = require('cors')
 
 // Start up an instance of app
 const app = express();
 
 /* Middleware */
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(express.json()); // Parse JSON payloads
+app.use(express.urlencoded({ extended: true })); // Parse URL-encoded form data
 app.use(cors());
 
 /* Initializing the main project folder */
@@ -25,5 +24,21 @@ function listing(){
     console.log(`running on localhost: ${port}`);
 }
 
+// Get route that return the projectData
+app.get('/data', (req, res) => {
+    res.send(projectData);
+    console.log(projectData);
+});
 
+// Post route that add incoming data to projectData
+app.post('/addData', (req, res) =>{
+    const { temp, date, user_res} = req.body;
+    const newEntry = {
+        temp,
+        date,
+        user_res
+    };
+    projectData = newEntry;
+    res.send({message: "Data added successfully"});
+});
 
