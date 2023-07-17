a923de0e3b61d692d4c2aa51c9a7b6f9629fd693
YousefKullab <the1yousef98@gmail.com>
2023-07-17 17:04:43 +0300

    update API to suitable to course requirement

--- website/app.js
+++ website/app.js
@@ -1,16 +1,17 @@
 /* Global Variables */
-api_key = "YOUR_API_KEY";
-base_url = "https://api.openweathermap.org/data/3.0/onecall";
+// api_key = "YOUR_API_KEY";
+api_key = "8d802503a466021106005166936d9817";
+base_url = "https://api.openweathermap.org/data/2.5/weather";
 
-// API: https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+// API: https://api.openweathermap.org/data/2.5/weather?zip={zip code},{country code}&appid={API key}
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth()+1 +'.'+ d.getDate()+'.'+ d.getFullYear();
 
 // Function to fetch data from open weather api
 const getData = async (zip_code) =>{
-    const api_url = `${base_url}?zip=${zip_code}&appid=${api_key}&unites=metric`;
+    const api_url = `${base_url}?zip=${zip_code},us&appid=${api_key}&units=metric`;
     try{
         const res = await fetch(api_url);
         if (!res.ok){
@@ -28,7 +29,7 @@ const getData = async (zip_code) =>{
 const postData = (url='', data={}) =>{
     return fetch(url, {
         method: "POST",
-        header: {
+        headers: {
             'Content-Type': 'application/json',
         },
         body: JSON.stringify(data),
@@ -65,21 +66,21 @@ const updateUI = () =>{
 };
 
 // Callback func when Generate btn is clicked
-const preformAction = () =>{
+const  preformAction = () =>{
     const zip_code = document.getElementById('zip').value;
     const feelings = document.getElementById('feelings').value;
 
     getData(zip_code)
-    .then(weather_data =>{
+    .then( weather_data =>{
         const data = {
             temp: weather_data.main.temp,
             date: newDate,
             user_res: feelings
         };
+        console.log(weather_data);
         return postData('/addData', data);
     })
     .then(res =>{
-        console.log(res);
         return updateUI();
     })
     .catch(error =>{
